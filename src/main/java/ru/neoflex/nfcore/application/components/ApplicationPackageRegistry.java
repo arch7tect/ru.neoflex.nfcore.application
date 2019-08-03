40d716cbbf92fc3cee98b2fdc429789e60ff57c6
arch7tect <arch7tect@gmail.com>
2019-08-04 01:43:54 +0300

    application registry

---
+++ src/main/java/ru/neoflex/nfcore/application/components/ApplicationPackageRegistry.java
@@ -0,0 +1,21 @@
+package ru.neoflex.nfcore.application.components;
+
+import org.eclipse.emf.ecore.EPackage;
+import org.springframework.stereotype.Component;
+import ru.neoflex.nfcore.application.ApplicationPackage;
+import ru.neoflex.nfcore.base.auth.AuthPackage;
+import ru.neoflex.nfcore.base.components.IPackageRegistry;
+import ru.neoflex.nfcore.base.types.TypesPackage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ApplicationPackageRegistry implements IPackageRegistry {
+    @Override
+    public List<EPackage> getEPackages() {
+        List<EPackage> result = new ArrayList<>();
+        result.add(ApplicationPackage.eINSTANCE);
+        return result;
+    }
+}
