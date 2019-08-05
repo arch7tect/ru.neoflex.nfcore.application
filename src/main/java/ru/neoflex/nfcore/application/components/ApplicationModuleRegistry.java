c24b4695ba562fb27bbca171da1443433a47db24
Oleg Orlov <orlov@neoflex.ru>
2019-08-05 17:17:47 +0300

    cleanup imports

--- src/main/java/ru/neoflex/nfcore/application/components/ApplicationModuleRegistry.java
+++ src/main/java/ru/neoflex/nfcore/application/components/ApplicationModuleRegistry.java
@@ -1,15 +1,10 @@
 package ru.neoflex.nfcore.application.components;
 
 import org.eclipse.emf.ecore.EPackage;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import ru.neoflex.nfcore.application.ApplicationPackage;
 import ru.neoflex.nfcore.base.components.IModuleRegistry;
-import ru.neoflex.nfcore.base.components.PackageRegistry;
 
-import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
