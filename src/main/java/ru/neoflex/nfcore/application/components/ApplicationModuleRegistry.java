c19a53a3bf1fdbc8e8a685f911089b99259e2aec
arch7tect <arch7tect@gmail.com>
2019-08-10 11:54:03 +0300

    add empty main class for annotations only

--- src/main/java/ru/neoflex/nfcore/application/components/ApplicationModuleRegistry.java
+++ src/main/java/ru/neoflex/nfcore/application/components/ApplicationModuleRegistry.java
@@ -1,6 +1,8 @@
 package ru.neoflex.nfcore.application.components;
 
 import org.eclipse.emf.ecore.EPackage;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import ru.neoflex.nfcore.application.ApplicationPackage;
 import ru.neoflex.nfcore.base.components.IModuleRegistry;
@@ -8,6 +10,8 @@ import ru.neoflex.nfcore.base.components.IModuleRegistry;
 import java.util.ArrayList;
 import java.util.List;
 
+@SpringBootApplication
+@ComponentScan("ru.neoflex.nfcore")
 @Component
 public class ApplicationModuleRegistry implements IModuleRegistry {
     @Override
