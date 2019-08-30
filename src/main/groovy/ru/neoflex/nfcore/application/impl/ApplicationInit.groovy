5040afe1d504633171d12d147ab92818796f1870
Oleg Orlov <orlov@neoflex.ru>
2019-08-30 18:48:34 +0300

    model changes

--- src/main/groovy/ru/neoflex/nfcore/application/impl/ApplicationInit.groovy
+++ src/main/groovy/ru/neoflex/nfcore/application/impl/ApplicationInit.groovy
@@ -4,6 +4,7 @@ import org.eclipse.emf.ecore.EClass
 import org.eclipse.emf.ecore.util.EcoreUtil
 import ru.neoflex.nfcore.application.ApplicationFactory
 import ru.neoflex.nfcore.application.ApplicationPackage
+import ru.neoflex.nfcore.application.CatalogNode
 import ru.neoflex.nfcore.base.services.Context
 import ru.neoflex.nfcore.base.util.DocFinder
 import ru.neoflex.nfcore.reports.ReportsPackage
@@ -30,21 +31,26 @@ class ApplicationInit {
             Context.current.store.deleteResource(rs.resources.remove(0).getURI())
         }
         if (rs.resources.empty) {
-            def eObject = ApplicationFactory.eINSTANCE.createApplication()
-            eObject.name = name
+            def application = ApplicationFactory.eINSTANCE.createApplication()
+            def decoratedView = ApplicationFactory.eINSTANCE.createDecoratedView()
+            application.setView(decoratedView)
+            application.name = name
             def referenceTree = ApplicationFactory.eINSTANCE.createCatalogNode()
             for (i in 1..5) {
                 def catalog = ApplicationFactory.eINSTANCE.createCatalogNode()
-                catalog.name = 'Catalog' + i
+                catalog.code = 'Catalog' + i
                 referenceTree.children.add(catalog)
             }
             def reportNode = ApplicationFactory.eINSTANCE.createEObjectNode()
-            reportNode.name = 'report1'
+            reportNode.code = 'report1'
             def report1 = findOrCreateEObject(ReportsPackage.Literals.REPORT, "report1", true)
-            reportNode.EObject = report1
-            referenceTree.children[0].children.add(reportNode)
-            eObject.setReferenceTree(referenceTree)
-            rs.resources.add(Context.current.store.createEObject(eObject))
+            reportNode.setEObject(report1)
+            (referenceTree.children[0] as CatalogNode).children.add(reportNode)
+            decoratedView.setReferenceTree(referenceTree)
+            def typography = ApplicationFactory.eINSTANCE.createTypography()
+            typography.text = "Hello From Reports!"
+            decoratedView.children.add(typography)
+            rs.resources.add(Context.current.store.createEObject(application))
         }
         return rs.resources.get(0).contents.get(0)
     }
